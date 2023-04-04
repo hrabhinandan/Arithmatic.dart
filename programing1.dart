8a0c2c538bebb926c6726ac1c47faeef6d617c0b
H R ABHINANDAN <96574240+hrabhinandan@users.noreply.github.com>
2023-04-04 17:52:56 +0530

    my first programming code
    hi , I am good to code form now onwards..!!!

---
+++ programing1.dart
@@ -0,0 +1,18 @@
+void main() 
+{
+int num1=10;
+int num2=3;
+int sum = num1 + num2;
+int diff = num1 - num2;
+int div = num1 / num2;
+int div2 = num1 ~/ num2;
+int mul = num1 * num2;
+int unaryMinus = -num2;
+int mod = num1 % num2;
+print("the addition of num1 and num2 is $sum ");
+print("the substraction of num1 and num2 is $diff ");
+print("the division of num1 and num2 is $div ");
+print("the integer division of num1 and num2 is $div2 ");
+print("the unaryMinus of num1 and num2 is $unaryMinus ");
+print("the mod of num1 and num2 is $mod ");
+}
